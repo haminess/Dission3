a967d93c162ba8242413717ec67fc026ddf6d591
smartrhj <smartrhj@gmail.com>
2023-07-21 22:16:40 +0900

    Chat
    add Chat

---
+++ Assets/Scripts/RHJ/RHJ_Scripts/NpcSentence.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcSentence : MonoBehaviour
+{
+    public string[] sentences;
+    public Transform chatTr;
+    public GameObject chatBoxPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    public void TalkNpc()
+    {
+        GameObject go = Instantiate(chatBoxPrefab);
+        go.GetComponent<ChatSystem>().Ondialogue(sentences, chatTr);
+    }
+
+    private void OnMouseDown()
+    {
+        TalkNpc();
+    }
+}
