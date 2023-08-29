c400e3136b514b20cc179b52e97f3b196bea1b02
haminess <113650650+haminess@users.noreply.github.com>
2023-08-29 17:44:19 +0900

    add note eff

---
+++ Assets/Scripts/Particlesys.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particlesys : MonoBehaviour
+{
+    public ParticleSystem particlesys;
+    public void playy()
+    {
+        particlesys.Play();
+    }
+}
