2238ab05504d0293b2c1f1db9ac00d24eb0776ad
haminess <113650650+haminess@users.noreply.github.com>
2023-08-30 10:08:27 +0900

    시작화면 UI 추가

--- Assets/Scripts/UI/SoundManager.cs
+++ Assets/Scripts/UI/SoundManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SoundManager : MonoBehaviour
 {
@@ -32,17 +33,17 @@ public class SoundManager : MonoBehaviour
         // 데이터 불러오기
         DataManager.Instance.LoadSoundData();
 
-        // 초기 값 세팅
-        if(GameObject.Find("Option"))
-        {
-            GameObject settingui = GameObject.Find("SettingUI");
-            Slider[] slider = settingui.GetComponentsInChildren<Slider>();
-            bgmslider = slider[0];
-            effectslider = slider[1];
+        // 로컬 음량 세팅
+        bgm.volume = sounddata.bgm;
+        effect.volume = sounddata.effect;
 
-            bgmslider.value = sounddata.bgm;
-            effectslider.value = sounddata.effect;
-        }
+        // 설정창
+        GameObject settingui = GameObject.Find("SettingUI");
+        Slider[] slider = settingui.GetComponentsInChildren<Slider>();
+        bgmslider = slider[0];
+        effectslider = slider[1];
+        bgmslider.value = bgm.volume;
+        effectslider.value = effect.volume;
     }
     public void SetBgm(int _num)
     {
@@ -130,5 +131,73 @@ public class SoundManager : MonoBehaviour
     {
         this.effect.volume = effect;
         sounddata.effect = effect;
+    }
+
+    // 슬라이더
+    public void AddMusicVol(float _plus)
+    {
+        float set = this.bgm.volume + _plus;
+        if (set > 1)
+            set = 1;
+        else if (set < 0)
+            set = 0;
+
+        bgmslider.value = set;
+        bgm.volume = set;
+    }
+    public void AddEffectVol(float _plus)
+    {
+        float set = this.effect.volume + _plus;
+        if (set > 1)
+            set = 1;
+        else if (set < 0)
+            set = 0;
+
+        effectslider.value = set;
+        effect.volume = set;
+    }
+    public void SubMusicVol(float _minus)
+    {
+        float set = this.bgm.volume - _minus;
+        if (set > 1)
+            set = 1;
+        else if (set < 0)
+            set = 0;
+
+        bgmslider.value = set;
+        bgm.volume = set;
+    }
+    public void SubEffectVol(float _minus)
+    {
+        float set = this.effect.volume - _minus;
+        if (set > 1)
+            set = 1;
+        else if (set < 0)
+            set = 0;
+
+        effectslider.value = set;
+        effect.volume = set;
+    }
+
+    public void ShowValue(TextMeshProUGUI _text)
+    {
+        if(_text.name == "bgmvolume")
+        {
+            _text.text = (bgm.volume * 100).ToString("000");
+        }
+        if (_text.name == "effectvolume")
+        {
+            _text.text = (effect.volume * 100).ToString("000");
+        }
+    }
+
+    public void ConnectSoundManager()
+    {
+        if(GameObject.Find("SoundManager"))
+        {
+            SoundManager sm = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+            bgm = sm.bgm;
+            effect = sm.effect;
+        }
     }
 }
