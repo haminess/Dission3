046e9f345b31c66582430e00d758eb8f4b1c4d3c
temmmmmmie <125906814+temmmmmmie@users.noreply.github.com>
2023-08-31 21:44:05 +0900

    fadeoutin

---
+++ Assets/Scripts/Fadeinout.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fadeinout : MonoBehaviour
+{
+    Animation anim;
+    public ChangeScene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void fadeinout()
+    {
+        StartCoroutine(wait());
+    }
+
+    IEnumerator wait()
+    {
+        anim.clip = anim.GetClip("fadeout");
+        anim.Play();
+        yield return new WaitForSeconds(0.8f);
+        scene.ToMainGameScene();
+        yield return new WaitForSeconds(0.5f);
+        anim.clip = anim.GetClip("fadein");
+        anim.Play();
+    }
+}
