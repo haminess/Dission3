887ef78d2b4e9295f7523d4d5d3f457a12a0a0b7
jwildkj <122768853+jwildkj@users.noreply.github.com>
2023-09-15 18:39:08 +0900

    회의 업데이트

--- Assets/Scripts/UI/CharacterManager_SJY.cs
+++ Assets/Scripts/UI/CharacterManager_SJY.cs
@@ -26,49 +26,60 @@ public class CharacterManager_SJY : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Debug.Log("Right");
-
-            if (CharacterNum - 2 < 0)
-            {
-                Character[CharacterNum + 3].Play("Character_Select_Right_1");
-                Debug.Log("Right1");
-            }
-            else { Character[CharacterNum - 2].Play("Character_Select_Right_1"); Debug.Log("Right1-1"); }
-            if (CharacterNum - 1 < 0)
-            {
-                Character[CharacterNum + 4].Play("Character_Select_Right_2");
-            }
-            else { Character[CharacterNum - 1].Play("Character_Select_Right_2"); }
-            Character[CharacterNum].Play("Character_Select_Right_3");
-            if (CharacterNum + 1 > 4)
-            {
-                Character[CharacterNum - 4].Play("Character_Select_Right_4");
-            }
-            else { Character[CharacterNum + 1].Play("Character_Select_Right_4"); }
-            if (CharacterNum == 0) { CharacterNum = 4; }
-            else { CharacterNum--; }
+            CharacterSelectRight();
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if (CharacterNum - 1 < 0)
-            {
-                Character[CharacterNum + 4].Play("Character_Select_Left_1");
-            }
-            else { Character[CharacterNum - 1].Play("Character_Select_Left_1"); }
-            Character[CharacterNum].Play("Character_Select_Left_2");
-            if (CharacterNum + 1 > 4)
-            {
-                Character[CharacterNum - 4].Play("Character_Select_Left_3");
-            }
-            else { Character[CharacterNum + 1].Play("Character_Select_Left_3"); }
-            if (CharacterNum + 2 > 4)
-            {
-                Character[CharacterNum - 3].Play("Character_Select_Left_4");
-            }
-            else { Character[CharacterNum + 2].Play("Character_Select_Left_4"); }
-            if (CharacterNum == 4 ) { CharacterNum = 0; }
-            else { CharacterNum++; }
+            CharacterSelectLeft();
         }
+
+    }
+
+    void CharacterSelectRight()
+    {
+        Debug.Log("Right");
+
+        if (CharacterNum - 2 < 0)
+        {
+            Character[CharacterNum + 3].Play("Character_Select_Right_1");
+            Debug.Log("Right1");
+        }
+        else { Character[CharacterNum - 2].Play("Character_Select_Right_1"); Debug.Log("Right1-1"); }
+        if (CharacterNum - 1 < 0)
+        {
+            Character[CharacterNum + 4].Play("Character_Select_Right_2");
+        }
+        else { Character[CharacterNum - 1].Play("Character_Select_Right_2"); }
+        Character[CharacterNum].Play("Character_Select_Right_3");
+        if (CharacterNum + 1 > 4)
+        {
+            Character[CharacterNum - 4].Play("Character_Select_Right_4");
+        }
+        else { Character[CharacterNum + 1].Play("Character_Select_Right_4"); }
+        if (CharacterNum == 0) { CharacterNum = 4; }
+        else { CharacterNum--; }
+    }
+
+    void CharacterSelectLeft()
+    {
+        if (CharacterNum - 1 < 0)
+        {
+            Character[CharacterNum + 4].Play("Character_Select_Left_1");
+        }
+        else { Character[CharacterNum - 1].Play("Character_Select_Left_1"); }
+        Character[CharacterNum].Play("Character_Select_Left_2");
+        if (CharacterNum + 1 > 4)
+        {
+            Character[CharacterNum - 4].Play("Character_Select_Left_3");
+        }
+        else { Character[CharacterNum + 1].Play("Character_Select_Left_3"); }
+        if (CharacterNum + 2 > 4)
+        {
+            Character[CharacterNum - 3].Play("Character_Select_Left_4");
+        }
+        else { Character[CharacterNum + 2].Play("Character_Select_Left_4"); }
+        if (CharacterNum == 4) { CharacterNum = 0; }
+        else { CharacterNum++; }
     }
 }
